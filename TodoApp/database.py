b6057ff4a560b4315f19aba12ec4e285f14c329c
ishansaxena1399 <101873605+ishansaxena1399@users.noreply.github.com>
2025-08-24 16:52:29 +0530

    production database change

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:ishan@localhost/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://neondb_owner:npg_IXljiOVMcm15@ep-tiny-surf-a1g3vj5h-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:ishan@127.0.0.1:3306/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
